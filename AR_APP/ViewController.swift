b03e0ebf1d91d693228bc86ae04c073547cd5fdc
yyokii <yyokii.h@gmil.com>
2017-10-29 03:15:47 +0900

    Initial Commit

---
+++ AR_APP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AR_APP
+//
+//  Created by Yoki on 2017/10/29.
+//  Copyright © 2017年 yoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
