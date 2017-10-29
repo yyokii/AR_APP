52b7e7e514a5808a620c406ccd99c90e4f92664a
yyokii <yyokii.h@gmil.com>
2017-10-30 01:39:16 +0900

    オブジェクトの配置

--- AR_APP/ViewController.swift
+++ AR_APP/ViewController.swift
@@ -7,19 +7,86 @@
 //
 
 import UIKit
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, ARSCNViewDelegate {
+
+    @IBOutlet weak var itemCollectionView: UICollectionView!
+    @IBOutlet weak var mainSceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    var selectedItem: String?
+    
+    let itemsArray: [String] = ["wine_glass","cup"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        initialize()
+        registerGestureRecognizers()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func initialize (){
+        
+        //ARSCNiew初期化
+        self.mainSceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.configuration.planeDetection = .horizontal
+        self.mainSceneView.session.run(configuration)
+        self.mainSceneView.delegate = self
+        self.mainSceneView.autoenablesDefaultLighting = true
+        
+        self.itemCollectionView.dataSource = self
+        self.itemCollectionView.delegate = self
     }
 
+    func registerGestureRecognizers() {
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        self.mainSceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func tapped(sender: UITapGestureRecognizer) {
+        let sceneView = sender.view as! ARSCNView
+        let tapLocation = sender.location(in: sceneView)
+        let hitTest = sceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent)
+        if !hitTest.isEmpty {
+            self.addItem(hitTestResult: hitTest.first!)
+        }
+    }
+    
+    func addItem(hitTestResult: ARHitTestResult) {
+        if let selectedItem = self.selectedItem {
+            let scene = SCNScene(named: "Models.scnassets/\(selectedItem).scn")
+            let node = (scene?.rootNode.childNode(withName: selectedItem, recursively: false))!
+            let transform = hitTestResult.worldTransform
+            let thirdColumn = transform.columns.3
+            node.position = SCNVector3(thirdColumn.x, thirdColumn.y, thirdColumn.z)
+            self.mainSceneView.scene.rootNode.addChildNode(node)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return itemsArray.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "item", for: indexPath) as! itemCell
+        cell.itemLabel.text = self.itemsArray[indexPath.row]
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        self.selectedItem = itemsArray[indexPath.row]
+        cell?.backgroundColor = UIColor.orange
+    }
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.green
+    }
 
+    
 }
 
---
+++ AR_APP/itemCell.swift
@@ -0,0 +1,13 @@
+//
+//  itemCell.swift
+//  AR_APP
+//
+//  Created by Yoki on 2017/10/29.
+//  Copyright © 2017年 yoki. All rights reserved.
+//
+
+import UIKit
+
+class itemCell: UICollectionViewCell {
+    @IBOutlet weak var itemLabel: UILabel!
+}
